9bea2f73a985521fa9db3407f012269cac174256
EllieThePink <133701085+elliethepink@users.noreply.github.com>
2025-11-21 12:36:49 +0000

    R122

--- src/apiConnector.ts
+++ src/apiConnector.ts
@@ -62,7 +62,7 @@ export interface RoomDefinition {
 }
 
 // What the bot advertises as its game version
-const GAMEVERSION = "R121";
+const GAMEVERSION = "R122";
 const LZSTRING_MAGIC = "╬";
 
 class PromiseResolve<T> {
