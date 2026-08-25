d97bc7d7a0006a1b4de0afebfec533cadcaa6d29
LillyBlueHair <LillyBlueHair@gmail.com>
2026-08-25 13:44:41 +0200

    update resetStrikes and add getStrikes

--- bin/games/casino.ts
+++ bin/games/casino.ts
@@ -152,6 +152,7 @@ export class Casino {
         this.commandParser.register("scoreboard", this.onCommandScoreboard);
         this.commandParser.register("color", this.onCommandColor);
         this.commandParser.register("refund", this.onCommandRefund);
+        this.commandParser.register("getstrikes", this.onCommandGetStrikes);
         this.commandParser.register("resetstrikes", this.onCommandResetStrikes);
         this.commandParser.register(
             "markcompleted",
@@ -760,6 +761,39 @@ ${forfeitsString()}
         }
     };
 
+    private onCommandGetStrikes = async (
+        sender: API_Character,
+        msg: BC_Server_ChatRoomMessage,
+        args: string[],
+    ) => {
+        if (!sender.IsRoomAdmin()) {
+            this.conn.reply(msg, "Sorry, you need to be an admin");
+            return;
+        }
+
+        if (args.length < 1) {
+            this.conn.reply(
+                msg,
+                "Please add the name/number of the player whose strikes you want to see",
+            );
+            return;
+        }
+        const target = this.conn.chatRoom.findCharacter(args[0]);
+        if (!target) {
+            this.conn.reply(
+                msg,
+                "I can't find that person. (They need to be in the room)",
+            );
+            return;
+        }
+
+        const player = await this.store.getPlayer(target.MemberNumber);
+        this.conn.reply(
+            msg,
+            `${target} (${target.MemberNumber}) has ${player.cheatStrikes} strikes`,
+        );
+    };
+
     private onCommandResetStrikes = async (
         sender: API_Character,
         msg: BC_Server_ChatRoomMessage,
@@ -786,13 +820,22 @@ ${forfeitsString()}
             return;
         }
 
+        let strikes = 0;
+        if (args.length == 2 && args[1]) {
+            strikes = Number.parseInt(args[1]);
+            if (isNaN(strikes)) {
+                this.conn.reply(msg, "Invalid amount of strikes.");
+                return;
+            }
+        }
+
         const player = await this.store.getPlayer(target.MemberNumber);
-        player.cheatStrikes = 0;
+        player.cheatStrikes = strikes;
 
         await this.store.savePlayer(player);
         this.conn.reply(
             msg,
-            `Successfully reset ${target} (${target.MemberNumber}) strikes to 0`,
+            `Successfully reset ${target} (${target.MemberNumber}) strikes to ${strikes}`,
         );
     };
 
